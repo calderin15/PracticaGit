c3157e52630843df4bcc7b62ca1ba7951934f08a
CODIGO INFORMATICO <arcs.andres@gmail.com>
2023-12-07 15:55:24 -0500

    Guardar cambios locales en MainPersona.java

--- src/Main/MainPersona.java
+++ src/Main/MainPersona.java
@@ -10,12 +10,11 @@ public class MainPersona {
 
         Persona[] lista = {new Persona("Andres Gomez", "Bogota DC", "(555) 111 667 3"),
                     new Persona("Maria Castro", "Bogota DC", "(555) 234 532 4"),
-                new Persona("Jhon Martinez", "Bogota DC", "(555) 745 765 5"),
-                new Persona("Miguel Miranda", "Bogota DC", "(555) 543 344 7"),
-                new Persona("Laura Padilla", "Bogota DC", "(555) 872 654 2"),
-                new Persona("Martha Luna", "Bogota DC", "(555) 456 123 9")
+                    new Persona("John Martinez", "Bogota DC", "(555) 745 765 5"),
+                    new Persona("Miguel Miranda", "Bogota DC", "(555) 543 344 7"),
+                    new Persona("Laura Padilla", "Bogota DC", "(555) 872 654 2"),
+                    new Persona("Martha Luna", "Bogota DC", "(555) 456 123 9")
         };
-
 
         for (Persona p: lista) {
             personas.add(p);
